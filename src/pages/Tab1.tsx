77eb353a0b3ad62cd5e757c03763170a6ed65692
FritzIan02 <frtzbalendez@gmail.com>
2024-03-08 10:37:39 +0800

    added changes

---
+++ src/pages/Tab1.tsx
@@ -0,0 +1,34 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab1.css';
+
+const Tab1: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Balendez</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Tab 1</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonCard>
+      <img alt="Silhouette of mountains" src="../src/Assets/Img/Fritz.jpg" />
+      <IonCardHeader>
+        <IonCardTitle>Fritz Handsome</IonCardTitle>
+        <IonCardSubtitle>Always</IonCardSubtitle>
+      </IonCardHeader>
+
+      <IonCardContent>Thank you!!</IonCardContent>
+    </IonCard>
+        <ExploreContainer name="" />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab1;
