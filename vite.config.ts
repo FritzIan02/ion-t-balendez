55d48aec48a0b0161558186a44b120c09c1dd14d
FritzIan02 <frtzbalendez@gmail.com>
2024-05-10 10:33:11 +0800

    changed the base name in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ion-t-evangelio/',
+  base: '/ion-t-balendez/',
   plugins: [
     react(),
     legacy()
