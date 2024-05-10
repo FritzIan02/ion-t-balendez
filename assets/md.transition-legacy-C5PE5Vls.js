de964c2d2a81940a8087315f8a933a071c4789ca
FritzIan02 <frtzbalendez@gmail.com>
2024-05-10 10:36:02 +0800

    Updates

--- assets/md.transition-legacy-C5PE5Vls.js
+++ assets/md.transition-legacy-C5PE5Vls.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C13z1HPi.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-RBkVtkKa.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
