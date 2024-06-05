ff3ab557c8e02844108be248fd8c20a7d2302dc0
FritzIan02 <frztbalendez@gmail.com>
2024-06-05 10:03:35 +0800

    Updates

--- assets/status-tap-BIVOVX_I.js
+++ assets/status-tap-BIVOVX_I.js
@@ -1,3 +1,3 @@
-import{r as a,f as i,b as c,w as d,s as l}from"./index-BqWaU8Pj.js";/*!
+import{r as a,f as i,b as c,w as d,s as l}from"./index-DYiYJlrC.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
